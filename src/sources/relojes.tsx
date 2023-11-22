2abbfa22986a7de1fb43567aafbddf4ebad06834
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-11-22 11:32:16 -0500

    Update relojes.tsx

--- src/sources/relojes.tsx
+++ src/sources/relojes.tsx
@@ -10,3 +10,16 @@ export const relojesSources: SourcesMap = {
       "https://reloj-alarma.es/embed/hora/Santiago_CL/CHILE+CONTINENTAL/#theme=1&color=4&ampm=0&showdate=0",
   },
 };
+
+import { SourcesMap } from ".";
+import { BsClockFill } from "react-icons/bs";
+
+export const relojesSources: SourcesMap = {
+  RELOJ_CHILE: {
+    slug: "RELOJ_CHILE",
+    name: "RELOJ CHILE",
+    titleIcons: [<BsClockFill key="clock" />],
+    iframeSrc:
+      "https://reloj-alarma.es/embed/#theme=1&color=3&ampm=0&showdate=1&sound=bells",
+  },
+};
