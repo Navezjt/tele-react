4bf7ad5345e30d06e7b6a4c4edc290f55f504eec
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-11-22 11:37:00 -0500

    Update relojes.tsx

--- src/sources/relojes.tsx
+++ src/sources/relojes.tsx
@@ -15,9 +15,9 @@ import { SourcesMap } from ".";
 import { BsClockFill } from "react-icons/bs";
 
 export const relojesSources: SourcesMap = {
-  RELOJ_CHILE: {
-    slug: "RELOJ_CHILE",
-    name: "RELOJ CHILE",
+  RELOJ_PERU: {
+    slug: "RELOJ_PERU",
+    name: "RELOJ PERU",
     titleIcons: [<BsClockFill key="clock" />],
     iframeSrc:
       "https://reloj-alarma.es/embed/hora/Lima_PE/PERU+CONTINENTAL/#theme=1&color=4&ampm=0&showdate=0",
