0c744f4454d2a3fc405aa988b515cd1c31468bfa
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-11-30 15:54:28 -0500

    Update index.ts

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'; 
 import { camarasSources } from './camaras';
 import { chileSources } from './chile';
 import { datosSources } from './datos';
