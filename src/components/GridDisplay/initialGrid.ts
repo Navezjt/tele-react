485c9c47b6afb98e150161c079e5bb1acc0d472a
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2025-01-24 10:35:18 -0500

    Update initialGrid.ts

--- src/components/GridDisplay/initialGrid.ts
+++ src/components/GridDisplay/initialGrid.ts
@@ -4,11 +4,11 @@ import { SourceNode } from '../../pages/monitor/types';
 export const defaultGrid: SourceNode[] = [
   { sourceSlug: 'nbc-news', uuid: uuidv4() },
   { sourceSlug: 'atv-mas', uuid: uuidv4() },
-  { sourceSlug: 'tn', uuid: uuidv4() },
+  { sourceSlug: 'hch-vivo', uuid: uuidv4() },
   {
     sourceSlug: '24HTVN_YT',
     uuid: uuidv4()
   },
-  { sourceSlug: 'hch-vivo', uuid: uuidv4() },
+  { sourceSlug: 'tn', uuid: uuidv4() },
   { sourceSlug: 'RELOJ_CHILE', uuid: uuidv4() }
 ];
